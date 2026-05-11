5f4641c7f04060acc6a7fb1467b8a2a7be454816
J. Luis A <felizalexita@gmail.com>
2026-05-10 23:32:17 -0600

    idk

--- src/presentation/components/GameOverOverlay.tsx
+++ src/presentation/components/GameOverOverlay.tsx
@@ -7,7 +7,6 @@ interface GameOverOverlayProps {
 export const GameOverOverlay: React.FC<GameOverOverlayProps> = ({ state, onRetry, onMainMenu }) => {
   const winnerText = state.winnerId === 'tie' ? 'DRAW' : `${state.winnerId === 'p1' ? 'PLAYER 1' : 'OPPONENT'} WINS`;
   const p1Derezzed = state.winnerId !== 'tie' && state.winnerId !== 'p1';
-
   return (
     <div className="menu-overlay game-over-overlay">
       <h1 className="start-title danger-text">{p1Derezzed ? 'IDENTITY DISC DEREZZED' : 'SYSTEM OVERRIDE'}</h1>
