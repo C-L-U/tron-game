99c2333876c4ba20ba1d09d78d5d97199ff92dc2
J. Luis A <felizalexita@gmail.com>
2026-05-10 23:33:11 -0600

    idk

--- src/core/application/GameEngine.ts
+++ src/core/application/GameEngine.ts
@@ -82,7 +82,6 @@ export class GameEngine {
 
     // Player 1
     const p1 = new LightCycle('p1', new Vector2D(this.grid.width * 0.2, this.grid.height * 0.5), Direction.RIGHT, '#00ffff', 1 * speedMultiplier);
-
     // Player 2 (or AI)
     let p2Color = '#ff8c00'; // Normal orange
     let p2Speed = 1 * speedMultiplier;
