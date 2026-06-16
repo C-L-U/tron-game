4d62d4757946a8ae44ae340b1e45c37c8df2e273
J. Luis A <felizalexita@gmail.com>
2026-06-15 20:43:03 -0600

    idk

--- src/core/application/GameEngine.ts
+++ src/core/application/GameEngine.ts
@@ -7,7 +7,6 @@ import { type RenderPort } from '../ports/RenderPort';
 import { type AudioPort } from '../ports/AudioPort';
 import type { Difficulty, OpponentType } from '../domain/entities/GameState';
 export class GameEngine {
-
   public state: GameState;
   public grid: Grid;
   public cycles: LightCycle[] = [];
